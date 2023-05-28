989ff870b50b207184aa24ffa0f2621f2d8fc965
Dion Scott <dionanthonyscott@gmail.com>
2023-05-28 03:16:15 -0500

    Finish array exercises for first couple of tests

--- ruby_basics/6_arrays/exercises/array_exercises.rb
+++ ruby_basics/6_arrays/exercises/array_exercises.rb
@@ -1,13 +1,16 @@
 def nil_array(number)
   # return an array containing `nil` the given number of times
+  return Array.new(number)
 end
 
 def first_element(array)
   # return the first element of the array
+  return array.first
 end
 
 def third_element(array)
   # return the third element of the array
+  return array[2]
 end
 
 def last_three_elements(array)
